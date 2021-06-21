4cd09a5249ef91c731b58824471a73860be98319
Len Chang <lamgo.chang@gmail.com>
2021-06-21 12:18:27 +0800

    create a router: task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  // Method 1
+  //   tasksService: TasksService;
+  //   constructor(tasksService: TasksService) {
+  //     this.tasksService = tasksService;
+  //   }
+
+  // Method 2: private
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks() {
+    return this.tasksService.getTasks();
+  }
+
+  // Example: Func. of Class
+  //   helloWorld() {
+  //     this.tasksService.doSomething();
+  //   }
+}
