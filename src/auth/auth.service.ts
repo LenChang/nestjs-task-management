1cd4569ccc95d23a8697ab894e277a61a66bc50e
Len Chang <lamgo.chang@gmail.com>
2021-07-22 19:47:47 +0800

    Add router: SignIn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,5 +12,15 @@ export class AuthService {
 
   signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepo.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { userName, password } = authCredentialsDto;
+
+    const user = await this.userRepo.findOne({ userName });
+
+    if (user && bcrypt.compareSync(password, user.password)) return 'success';
+
+    throw new UnauthorizedException('Please check your credentials');
   }
 }
